693fcf2cc501049aa45c499e2422fba1d1d80015
jackrwoods <jackrwoods@gmail.com>
2018-04-20 11:55:58 -0700

    Update listserv signup email address.

--- joinMailingList.php
+++ joinMailingList.php
@@ -2,8 +2,8 @@
 echo "Sending email...";
 if ($_POST['email'] != "Your_Email@oregonstate.edu") {
 	// Send an email to marketing employee to register user for the mailing list.
-	$marketingEmailAddress = "bailey.wood@oregonstate.edu";
-	$name = "Bailey";
+	$marketingEmailAddress = "jack.woods@oregonstate.edu";
+	$name = "Jack";
 	$subject = "New Jeopardy Listserv Signup";
 	$message = "Hi ".$name."!<br /><br />A new user has requested to sign up for the Sustainability listserv! Here's their information:<br /><b>Location:</b> ".file_get_contents("http://freegeoip.net/csv/".$_SERVER['REMOTE_ADDR'])."<br /><b>Email:</b> ".$_POST['email']."<br /><b>Name:</b> ".$_POST['fullname']."<br /><br />Have a great day!<br />Jack's Listserv Email Script";
 	$headers = "From: DoNotReply@fa.oregonstate.edu\r\n";
