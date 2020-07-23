2a83c5b65d9fb2312643922c29f966bc0ea77185
Jack Woods <jackrwoods@gmail.com>
2020-07-22 22:18:46 -0700

    Finish initial jeopardy remake

--- src/components/Jeopardy_Board.vue
+++ src/components/Jeopardy_Board.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <el-row v-for="pointValue in [100, 200, 300, 400, 500]" :key="pointValue">
-      <el-col :span="4" v-for="category in categories" :key="category">
-        <question :category="category" :points="pointValue" />
+      <el-col :span="8" v-for="category in categories" :key="category">
+        <center><h1 v-if="pointValue === 100">{{ category }}</h1></center>
+        <question v-bind:category="category" v-bind:points="pointValue" />
       </el-col>
+    </el-row>
+    <el-row :style="{textAlign: 'center', margin: '.5em', fontSize: '1.5em'}">
+      Your Score: {{points}}
     </el-row>
   </div>
 </template>
@@ -16,8 +20,7 @@ export default {
   computed: {
     ...mapGetters([
       'categories',
-      'points',
-      'questions'
+      'points'
     ])
   },
   components: {
--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,21 +1,86 @@
 <template>
-  <div>
-    {{points}}
-  </div>
+<div>
+  <el-card :class="{ active: !answered, answered: answered, 'box-card': true }" shadow="hover" @click.native="dialogVisible = !answered">
+    <h1 :style="{ lineHeight: '8vh' }">{{ points }}</h1>
+  </el-card>
+  <!-- Hidden question dialog -->
+  <el-dialog
+    :title="category + ' for ' + points + ' Points'"
+    :visible.sync="dialogVisible"
+    width="50%"
+    center>
+    <div style="word-break: normal; text-align: center;">
+      {{ questionData.questionText }}
+    </div>
+    <span slot="footer" class="dialog-footer">
+      <el-radio-group @change="handleAnswer" v-model="selectedAnswer" size="mini">
+        <el-radio border v-for="(answer, index) in questionData.answers" :key="index" :label="index" @click="handleAnswer">{{ answer.answerText }}</el-radio>
+      </el-radio-group>
+    </span>
+  </el-dialog>
+</div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 export default {
   name: 'Question',
   computed: {
-    ...mapGetters([
-      'points'
-    ])
+    questionData: function () {
+      return this.$store.getters.questions(this.category)[0].questions.filter(question => question.pointValue === this.points)[0]
+    }
+  },
+  data () {
+    return {
+      answered: false,
+      dialogVisible: false,
+      selectedAnswer: null
+    }
+  },
+  methods: {
+    handleAnswer: function () {
+      this.answered = true
+      if (this.questionData.answers[this.selectedAnswer].isCorrect) {
+        this.$store.commit('addPoints', this.points)
+        this.$message({
+          message: 'Correct!',
+          type: 'success',
+          duration: 3000
+        })
+      } else {
+        this.$message.error('Incorrect! The correct answer was: "' + this.questionData.answers.filter(answer => answer.isCorrect)[0].answerText + '"', 3000)
+      }
+      this.dialogVisible = false
+    }
+  },
+  props: {
+    category: String,
+    points: Number
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
+@import '@/assets/style-variables.scss';
 
+.box-card {
+  text-align: center;
+  height: 13vh;
+  margin: .5em;
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+}
+.active {
+  cursor: pointer;
+}
+.active:hover {
+  border-color: $--color-primary;
+}
+.answered {
+  background-color: rgb(211,211,211);
+}
 </style>
